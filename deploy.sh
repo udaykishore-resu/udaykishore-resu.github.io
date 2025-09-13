c6172fb512778ff224470f43125e2e31fcde0847
udaykishore-resu <166478692+udaykishore-resu@users.noreply.github.com>
2025-09-12 23:22:43 -0400

    commit the deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+cd ../uday-kishore-folio
+npm install
+npm run build
+
+# Copy the built files to the GitHub Pages repo
+cp -r dist/* ../udaykishore-resu.github.io/
+
+# Navigate to the GitHub Pages repo
+cd ../udaykishore-resu.github.io
